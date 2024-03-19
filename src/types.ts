e41cdf3ac1af45204cd9d08f191d93ea0753090d
Cato Cannizzo <catocannizzo@gmail.com>
2024-03-19 14:42:13 -0400

    Finished content collections switch, fixed navbar for dark mode

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,6 @@
 //Delete or figure out how to use
 //explictly defines blogpost props for typescript
+//moving to collections I think is is no longer used but can be used to fix "post errors"
 interface BlogPostFrontMatter {
    title: string;
    date: string;
